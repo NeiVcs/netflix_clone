db5ca75c0decb9a2767a568d68ffc03d5964db90
NeiVcs <62248889+NeiVcs@users.noreply.github.com>
2021-11-04 17:53:19 -0300

    projeto front impulso

---
+++ js/owl/setup.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
